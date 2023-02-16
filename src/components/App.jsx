2dae936283254dbfff2d4ec7e2cf99914db5f7b5
Maryna <yasinskamarina@gmail.com>
2023-02-16 22:42:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import './styles.css';
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
